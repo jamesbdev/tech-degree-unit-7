3b12ce93d499675a7595d295f5a108ad48c670af
James Bremner <j.bremner@ymail.com>
2023-12-21 15:43:45 +0000

    add mobile chart

--- script/app.js
+++ script/app.js
@@ -11,16 +11,21 @@ closeIcon.addEventListener('click', () => {
 //charts 
 
 const trafficChart = document.querySelector('#traffic-chart');
+const dailyTraffic = document.querySelector('#daily-traffic');
+const mobileUser = document.querySelector('#mobile-user');
 
-
+//traffic chart
 new Chart(trafficChart, {
     type: 'line',
     data: {
         labels: ["12-1", "1-2", "2-3","3-4", "4-5", "5-6"],
         datasets: [{
-            label: '# of views',
+            label: 'Number of views',
             data: [500, 2000, 1500, 2000, 2500, 3000],
-            borderWidth: 1
+            borderWidth: 1,
+            borderColor: '#9b86dc',
+            backgroundColor: '#9b86dc',
+            color: '#9b86dc'
         }]
     },
     options: {
@@ -29,7 +34,41 @@ new Chart(trafficChart, {
                 beginAtZero: true
             }
         }
+  
+
     }
 })
+
+//Daily traffic chart
+
+new Chart(dailyTraffic, {
+    type: 'bar',
+    data: {
+        labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+        datasets: [{
+            label: 'Number of views',
+            data: [100, 200, 300, 400, 500, 600],
+            borderWidth: 1
+        }]
+    }
+})
+
+
+//mobile user chart
+
+new Chart(mobileUser, {
+    type: 'doughnut',
+    data: {
+        labels: ['Desktop', 'Tablet', 'Phones'],
+        datasets: [{
+            label: 'Number of views',
+            data: [100, 200, 300],
+            borderWidth: 1
+        }]
+    }
+})
+
+
+
 
 
