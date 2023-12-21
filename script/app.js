aaceb8d8ffb7da1c88d0641886add4185bdb04de
James Bremner <j.bremner@ymail.com>
2023-12-21 11:36:12 +0000

    add first graph

--- script/app.js
+++ script/app.js
@@ -7,3 +7,29 @@ closeIcon.addEventListener('click', () => {
     }
   
 });
+
+//charts 
+
+const trafficChart = document.querySelector('#traffic-chart');
+
+
+new Chart(trafficChart, {
+    type: 'line',
+    data: {
+        labels: ["12-1", "1-2", "2-3","3-4", "4-5", "5-6"],
+        datasets: [{
+            label: '# of views',
+            data: [500, 2000, 1500, 2000, 2500, 3000],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+})
+
+
